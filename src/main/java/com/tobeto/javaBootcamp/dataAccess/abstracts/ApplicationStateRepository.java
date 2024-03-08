13f259af94cd8976c8ee66bab8f88068c1c56039
şükran sinci <80584011+devsukransinci@users.noreply.github.com>
2024-03-08 21:01:41 +0300

    Update ApplicationStateRepository.java

--- src/main/java/com/tobeto/javaBootcamp/dataAccess/abstracts/ApplicationStateRepository.java
+++ src/main/java/com/tobeto/javaBootcamp/dataAccess/abstracts/ApplicationStateRepository.java
@@ -3,7 +3,7 @@ package com.tobeto.javaBootcamp.dataAccess.abstracts;
 import com.tobeto.javaBootcamp.entities.concretes.Application;
 import com.tobeto.javaBootcamp.entities.concretes.ApplicationState;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//getDateEklenecek
 public interface ApplicationStateRepository extends JpaRepository<ApplicationState,Integer> {
     ApplicationState  getByName(String name);
     ApplicationState  getById(int id);
